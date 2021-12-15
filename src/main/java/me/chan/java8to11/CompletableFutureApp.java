6b41d71483a38f97d4f06def257c028cb87fb3b1
HyeokChan <qpjiq123@gmail.com>
2021-12-15 23:49:19 +0900

    Executors

--- src/main/java/me/chan/java8to11/CompletableFutureApp.java
+++ src/main/java/me/chan/java8to11/CompletableFutureApp.java
@@ -1,12 +1,17 @@
 package me.chan.java8to11;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class CompletableFutureApp {
 
     public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(() -> {
             System.out.println("Thread: " + Thread.currentThread().getName());
             try {
-                Thread.sleep(3000L);
+                Thread.sleep(1000L);
             } catch (InterruptedException e) {
                 throw new IllegalStateException(e);
             }
@@ -18,7 +23,34 @@ public class CompletableFutureApp {
         thread.join();
         System.out.println(thread + "is finished");
 
-        
+        // Executors
+        System.out.println("Executors >>");
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.submit(() -> {
+            System.out.println("Thread " + Thread.currentThread().getName());
+        });
+        executorService.shutdown();
 
+        ExecutorService executorService1 = Executors.newFixedThreadPool(2);
+        executorService1.submit(getRunnable("Hello"));
+        executorService1.submit(getRunnable("Keesun"));
+        executorService1.submit(getRunnable("Chan"));
+        executorService1.submit(getRunnable("JAVA"));
+        executorService1.submit(getRunnable("Thread"));
+        executorService1.shutdown();
+
+        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService.schedule(getRunnable("HelloWorld"), 3, TimeUnit.SECONDS);
+        scheduledExecutorService.shutdown();
+
+        ScheduledExecutorService scheduledExecutorService1 = Executors.newSingleThreadScheduledExecutor();
+        scheduledExecutorService1.scheduleAtFixedRate(getRunnable("Period"), 3, 2, TimeUnit.SECONDS);
+        scheduledExecutorService1.shutdown(); // Period 확인하려면 주석처리
+
+
+    }
+
+    private static Runnable getRunnable(String message) {
+        return () -> System.out.println(message + " : " + Thread.currentThread().getName());
     }
 }
