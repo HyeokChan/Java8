58a78317f3b4233818be00670ea99eaa2cb464c8
HyeokChan <qpjiq123@gmail.com>
2021-12-19 15:58:56 +0900

    배열 병렬 정렬

--- src/main/java/me/chan/java8to11/EtcApp.java
+++ src/main/java/me/chan/java8to11/EtcApp.java
@@ -2,11 +2,14 @@ package me.chan.java8to11;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.IntStream;
 
 @Chicken("양념")
 @Chicken("마늘간장")
 public class EtcApp {
     public static void main(String[] args) {
+        // 어노테이션의 변화
         Chicken[] chickens = EtcApp.class.getAnnotationsByType(Chicken.class);
         Arrays.stream(chickens).forEach(c -> {
             System.out.println("c = " + c.value());
@@ -16,6 +19,21 @@ public class EtcApp {
         Arrays.stream(chickenContainer.value()).forEach(c -> {
             System.out.println("c = " + c.value());
         });
+
+        // 배열 병렬 정렬
+        int size = 1500;
+        int[] numbers = new int[size];
+        Random random = new Random();
+
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        long start = System.nanoTime();
+        Arrays.sort(numbers);
+        System.out.println("serial sorting took " + (System.nanoTime() - start));
+
+        IntStream.range(0, size).forEach(i -> numbers[i] = random.nextInt());
+        start = System.nanoTime();
+        Arrays.parallelSort(numbers);
+        System.out.println("parallel sorting took " + (System.nanoTime() - start));
 
 
     }
